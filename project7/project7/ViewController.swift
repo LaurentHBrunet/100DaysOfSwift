2042558272d661ba4c2491837c5cc79f1bc90135
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2019-04-13 00:20:14 -0400

    day 35

--- project7/project7/ViewController.swift
+++ project7/project7/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetitions))
         
         let urlString: String
         if navigationController?.tabBarItem.tag == 0 {
@@ -31,6 +36,34 @@ class ViewController: UITableViewController {
         showError()
     }
     
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "Data pulled from we the people, white house api", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterPetitions() {
+        var textField: UITextField!
+        
+        let ac = UIAlertController(title: "Filter", message: nil, preferredStyle: .alert)
+        ac.addTextField { (UITextField) in
+            textField = UITextField
+        }
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            self.filteredPetitions.removeAll()
+            
+            for var petition in self.petitions {
+                if petition.title.contains(textField.text!) {
+                    self.filteredPetitions.append(petition)
+                }
+            }
+            self.tableView.reloadData()
+        }))
+        
+        present(ac, animated: true)
+    }
+    
     func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check connection", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -42,19 +75,20 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            print(petitions.count)
+            filteredPetitions = jsonPetitions.results
+            
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         
@@ -63,7 +97,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         
         navigationController?.pushViewController(vc, animated: true)
     }
