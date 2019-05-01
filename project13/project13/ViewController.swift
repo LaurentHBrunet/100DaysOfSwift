1e0b32414eda275388b6d065a6863906a270c31a
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2019-04-30 21:05:49 -0400

    day 52

---
+++ project13/project13/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Laurent on 2019-04-30.
+//  Copyright © 2019 Laurent Henault-Brunet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        
+        currentImage = image
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
