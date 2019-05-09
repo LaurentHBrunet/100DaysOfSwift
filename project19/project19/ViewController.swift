cdbc2b45d71653e974704db8fd2a06c26e457847
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2019-05-09 14:15:23 -0400

    day 67

---
+++ project19/project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project19
+//
+//  Created by Laurent on 2019-05-09.
+//  Copyright © 2019 Laurent Henault-Brunet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
