c70340797d149426c07a3f8f4c44fd90ffb1b655
Laurent Hénault-Brunet <laurent.henault.brunet@gmail.com>
2019-05-12 18:08:53 -0400

    day 69

--- project19/Extension/ActionViewController.swift
+++ project19/Extension/ActionViewController.swift
@@ -20,11 +20,13 @@ class ActionViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showPredefinedOptions))
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         
+
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
@@ -34,10 +36,22 @@ class ActionViewController: UIViewController {
                     self?.pageTitle = javaScriptValues["title"] as? String ?? ""
                     self?.pageURL = javaScriptValues["URL"] as? String ?? ""
                     
+                    self?.reloadScriptContent()
+                    
                     DispatchQueue.main.async {
                         self?.title = self?.pageTitle
                     }
                 }
+            }
+        }
+    }
+    
+    func reloadScriptContent() {
+        let defaults = UserDefaults.standard
+        
+        if let savedScript = defaults.object(forKey: pageURL) as? Data {
+            if let decodedScriptText = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedScript) as? String {
+                script.text = decodedScriptText
             }
         }
     }
@@ -61,11 +75,39 @@ class ActionViewController: UIViewController {
     }
 
     @IBAction func done() {
+        save()
         let item = NSExtensionItem()
         let argument: NSDictionary = ["customJavaScript": script.text]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
         item.attachments = [customJavaScript]
         extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    @objc func showPredefinedOptions() {
+        let ac = UIAlertController(title: "Choose predefined script", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Alert Hello world", style: .default, handler: { (UIAlertAction) in
+            self.doneWith(input: "alert(\"hello, world\")")
+        }))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.leftBarButtonItem
+
+        present(ac, animated: true)
+    }
+    
+    func doneWith(input: String) {
+        save()
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": input]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    func save() {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: script.text, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: pageURL)
+        }
     }
 }
